29cd33c251ef224c1a506804e22efd214cd7610b
Ravil <ravil.vasilev@yandex.ru>
2025-02-28 15:49:42 +0300

    Создал сервис для загрузки данных по матчам

---
+++ src/services/fetchMatches.ts
@@ -0,0 +1,51 @@
+import { TMatchStatus } from '../components/CardStatus/types.ts';
+import useSWR from 'swr';
+
+export const BASE_API_URL = 'https://app.ftoyd.com/fronttemp-service';
+
+export interface MatchItem {
+  awayScore: number;
+  awayTeam: Team;
+  homeScore: number;
+  homeTeam: Team;
+  status: TMatchStatus;
+  time: string;
+  title: string;
+}
+
+export interface Player {
+  kills: number;
+  username: string;
+}
+export interface Team {
+  name: string;
+  place: number;
+  players: Player[];
+  points: number;
+  total_kills: number;
+}
+
+export interface ApiMatchesResponse {
+  ok: boolean;
+  data: {
+    matches: MatchItem[];
+  };
+}
+
+const fetchMatches = async (): Promise<ApiMatchesResponse> => {
+  const response = await fetch(`${BASE_API_URL}/fronttemp`);
+  return response.json();
+};
+
+export function useMatches() {
+  const { data, error, isLoading } = useSWR<ApiMatchesResponse>(
+    `matches`,
+    fetchMatches,
+  );
+
+  return {
+    matches: data,
+    isLoading,
+    error,
+  };
+}
