dcc74b0fbf769d9b461bb3923995d007aad88611
Ravil <ravil.vasilev@yandex.ru>
2025-02-28 17:06:57 +0300

    Создал компонент для подгрузки и отображения списка матчей

---
+++ src/components/MatchList/MatchList.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import cls from 'classnames';
+import sx from './MatchList.module.css';
+import { useMatches } from '../../services/fetchMatches.ts';
+import { MatchCard } from '../MatchCard/MatchCard.tsx';
+import { MatchCardSkeleton } from '../MatchCard/MatchCardSkeleton.tsx';
+
+interface MatchListProps {
+  className?: string;
+}
+
+export const MatchList = (props: MatchListProps) => {
+  const { matches, isLoading, error } = useMatches();
+
+  if (isLoading) {
+    return <MatchCardSkeleton />;
+  }
+
+  if (!matches || error) {
+    return null;
+  }
+
+  const { matches: matchesList } = matches.data;
+  const { className } = props;
+  return (
+    <ul className={cls(sx.MatchList, className, 'container')}>
+      {matchesList.map((item) => (
+        <li key={item.title}>
+          <MatchCard data={item} />
+        </li>
+      ))}
+    </ul>
+  );
+};
