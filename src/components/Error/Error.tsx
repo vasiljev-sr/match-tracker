1462c532611605c9b7ade9bcd8f96fb56abf77fa
Ravil <ravil.vasilev@yandex.ru>
2025-02-28 17:05:12 +0300

    Добавил компонент Error

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,24 @@
+import sx from './Error.module.css';
+import cls from 'classnames';
+import image from '../../assets/images/error.svg';
+import { useMatches } from '../../services/fetchMatches.ts';
+interface ErrorProps {
+  className?: string;
+}
+export const Error = (props: ErrorProps) => {
+  const { className } = props;
+
+  const { error } = useMatches();
+
+  if (!error) {
+    return null;
+  }
+  return (
+    <div className={cls(sx.Error, className)}>
+      <img src={image} alt="Error" />
+      <p>Ошибка: не удалось загрузить информацию</p>
+    </div>
+  );
+};
+
+export default Error;
